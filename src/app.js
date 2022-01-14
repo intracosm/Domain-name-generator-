d5843e2e1ebf0d3363ac57c66a2b23c9b2a80043
intracosm <jmeprivert91@gmail.com>
2022-01-14 17:06:35 +0000

    finished generating

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".org"];
   console.log("Hello Rigo from the console!");
+
+  let domains = [];
+
+  for (let pronoun of pronoun) {
+    for (let adj of adj) {
+      for (let noun of noun) {
+        for (let extensions of extensions) {
+          document.querySelector(
+            "#domain-list"
+          ).innerHTML += `<li> ${pronoun}${adj}${noun}${extensions}</li>`;
+        }
+      }
+    }
+  }
+  console.log(domains);
 };
